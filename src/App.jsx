086dcbc69476c26ab287e577cadfc2cf072bd377
DevL4vuie <pacaldolouige@gmail.com>
2026-03-20 14:10:11 +0800

    Switched to HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import Layout from './components/Layout';
@@ -11,6 +11,7 @@ import POS from './pages/POS.jsx';
 import Products from './pages/Products';
 import Sales from './pages/Sales'; // Make sure you created a dummy file for this!
 import Practice from './pages/Practice.jsx';
+
 function App() {
   return (
     <AuthProvider>
@@ -18,14 +19,15 @@ function App() {
         <Routes>
           {/* Public Route - No Sidebar here */}
           <Route path="/login" element={<Login />} />
- <Route path="/practice" element={<Practice />} />
+          <Route path="/practice" element={<Practice />} />
+          
           {/* Protected Routes wrapped in the Sidebar Layout */}
           <Route path="/" element={<ProtectedRoute><Layout /></ProtectedRoute>}>
             <Route index element={<Dashboard />} />
             <Route path="pos" element={<POS />} />
             <Route path="products" element={<Products />} />
             <Route path="sales" element={<Sales />} />
-              <Route path="practice" element={<Practice />} />
+            <Route path="practice" element={<Practice />} />
           </Route>
         </Routes>
       </Router>
